1ccdf663b677023c34f4c919d42a907271a562fa
EC2 Default User <ec2-user@ip-172-31-46-221.ap-northeast-1.compute.internal>
2020-05-26 15:17:23 +0000

    Add Blog

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :blogs
   resources :tops
   #root 'messages#index'
   resources :ti_tles
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200526140632) do
+ActiveRecord::Schema.define(version: 20200526151211) do
+
+  create_table "blogs", force: :cascade do |t|
+    t.string   "title"
+    t.string   "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "messages", force: :cascade do |t|
     t.string   "title"
